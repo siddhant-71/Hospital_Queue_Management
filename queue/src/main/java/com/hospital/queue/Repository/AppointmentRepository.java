a0a16787b3badb02fe135d06cbba4024843bdbdd
Siddhant Balasaheb Devkar <siddhantd711@gmail.com>
2025-09-14 18:06:38 +0530

    Delete queue/src/main/java/com/hospital/queue/Repository directory

--- queue/src/main/java/com/hospital/queue/Repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.queue.Repository;
-
-import com.hospital.queue.Entities.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
-}
--- queue/src/main/java/com/hospital/queue/Repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.queue.Repository;
-
-import com.hospital.queue.Entities.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-}
