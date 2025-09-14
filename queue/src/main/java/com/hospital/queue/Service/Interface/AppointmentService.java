df7cabd732b6e214050b329dda377ebaf613df17
Siddhant Balasaheb Devkar <siddhantd711@gmail.com>
2025-09-14 18:06:47 +0530

    Delete queue/src/main/java/com/hospital/queue/Service directory

--- queue/src/main/java/com/hospital/queue/Service/Interface/AppointmentService.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.queue.Service.Interface;
-
-import com.hospital.queue.DTO.AppointmentBookedResponse;
-import com.hospital.queue.Entities.Appointment;
-
-public interface AppointmentService {
-    AppointmentBookedResponse bookAppointment(Long doctorId, Long patientId, int slot);
-    boolean cancelAppointment(Long doctorId, Long patientId, int slot,Long id,int i);
-}
