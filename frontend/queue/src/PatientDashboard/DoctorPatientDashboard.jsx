1019a0f1ec349e41b5b71323ff683ec4838b4337
Siddhant Balasaheb Devkar <siddhantd711@gmail.com>
2025-09-15 18:12:41 +0530

    Delete frontend/queue/src/PatientDashboard directory

--- frontend/queue/src/PatientDashboard/DoctorPatientDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DoctorPatientDashboard = () => {
-  return (
-    <div>DoctorPatientDashboard</div>
-  )
-}
-
-export default DoctorPatientDashboard
--- frontend/queue/src/PatientDashboard/HospitalPatientDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HospitalPatientDashboard = () => {
-  return (
-    <div>HospitalPatientDashboard</div>
-  )
-}
-
-export default HospitalPatientDashboard
